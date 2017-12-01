08af91a444bfd68f0c0673a815fbb2606c5185bc
ed131219 <hahahaggg@gmail.com>
2017-12-01 21:24:15 +0800

    Change
    初始化
    scanBlock
    deleteBlockRow

--- Tetris/TetrisModel.cs
+++ Tetris/TetrisModel.cs
@@ -41,17 +41,24 @@ namespace Tetris
             public CUBE cube = new CUBE();
             public CUBE_STATE cube_state = new CUBE_STATE();
 
+            public Block()
+            {
+                cube = (CUBE ) (0);
+                cube_state = (CUBE_STATE) (0);
+            }
             public Block(CUBE c, CUBE_STATE cs)
             {
                 cube = c;
                 cube_state = cs;
             }
+            
         }
 
         private STATE nowState;
         private Block[,] block;
         private Block[,] nextBlock;
-
+        private int blockx = 20, blocky = 10;
+        private int nextBlockx = 4, nextBlocky = 4;
         private int timer;
         private int score;
         private int level;
@@ -66,10 +73,10 @@ namespace Tetris
         public void InitAll()
         {
             nowState = STATE.STOP;
-
             // new Block and init them to CUBE.NONE
             // TODO : ...
-
+            block = new Block[blockx,blocky];//遊戲BLOCK 10*20
+            nextBlock = new Block[nextBlockx, nextBlocky];//下個方塊 4*4
 
             NextCreate();
 
@@ -151,15 +158,41 @@ namespace Tetris
         {
 
         }
+        // 判斷方塊有沒有HIT
+        public bool CubeHit() {
 
+            return false;
+        }
+        // 掃描需要消除的ROW
         public void BlockScan()
         {
-
+            if (CubeHit())
+            {
+                for (int i = 0; i < blockx; i++)
+                {
+                    for (int j = 0; j < blocky; j++)
+                    {
+                        if (block[i, j].cube != (CUBE)(1))
+                        {
+                            break;
+                        }
+                        if (j == (blocky - 1))
+                        {
+                            DeleteBlockRow(i);
+                        }
+                    }
+                }
+            }
         }
-
-        public void DeleteBlockRow(int i)
+        // 消除ROW並加分數
+        public void DeleteBlockRow(int x)
         {
-
+            int sc = 0,point = 10;  
+                for (int j = 0; j < 10; j++) {
+                    block[x, j] = new Block((CUBE)(0), (CUBE_STATE)(4));
+                }
+            sc = sc + point ;
+            SetScore(sc);
         }
 
         public void CubeCreate()
